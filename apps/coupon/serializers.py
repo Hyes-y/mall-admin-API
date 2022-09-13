2a62364fb3ad9f76fb4cffff06906f29157d5031
Hyes-y <gptjs7970@naver.com>
2022-09-14 03:47:00 +0900

    feat: CouponTypeSerializer, CouponTypeViewSet 구현 - 쿠폰 타입 생성시 필요한 값들에 대한 validate 구현 - 그 외 기능은 기본 기능으로 구현

--- apps/coupon/serializers.py
+++ apps/coupon/serializers.py
@@ -1,6 +1,36 @@
 from rest_framework.serializers import ModelSerializer
 from rest_framework.serializers import ValidationError
-from .models import Coupon
+from .models import Coupon, CouponType
+
+
+class CouponTypeSerializer(ModelSerializer):
+    """ 쿠폰 타입 관련 시리얼라이저 """
+    class Meta:
+        model = CouponType
+        fields = '__all__'
+
+    def validate(self, data):
+        start_date = data.get('start_date', None)
+        end_date = data.get('end_date', None)
+        period = data.get('period', None)
+        dc_type = data.get('dc_type', None)
+        iss_type = data.get('iss_type', None)
+        amount = data.get('amount', None)
+        code = data.get('code', None)
+
+        if start_date > end_date:
+            raise ValidationError("ERROR: 유효 기간이 올바르지 않습니다.")
+
+        if period < 0:
+            raise ValidationError("ERROR: 유효 기한이 올바르지 않습니다.")
+
+        if dc_type not in (0, 1, 2, 3) or iss_type not in (0, 1, 2):
+            raise ValidationError("ERROR: 올바르지 않은 쿠폰 종류입니다.")
+
+        if iss_type == 1 and not (amount and code):
+            raise ValidationError("ERROR: 사용자 발급의 경우 수량과 쿠폰 코드를 지정해주세요.")
+
+        return data
 
 
 class CouponSerializer(ModelSerializer):
--- apps/coupon/views.py
+++ apps/coupon/views.py
@@ -3,10 +3,15 @@ from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAdminUser
 # local modules
-from .models import Coupon
-from .serializers import CouponSerializer
+from .models import Coupon, CouponType
+from .serializers import CouponSerializer, CouponTypeSerializer
+
+
+class CouponTypeViewSet(viewsets.ModelViewSet):
+    queryset = CouponType.objects.all()
+    serializer_class = CouponTypeSerializer
+    # permission_classes = [IsAdminUser]
 
 
 class CouponViewSet(viewsets.ModelViewSet):
     pass
-
